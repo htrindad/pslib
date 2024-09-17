3cd6c4bb0e866485f2add3c98b211c3ed63d5527
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-09-17 11:43:38 +0100

    nwlib

--- pslib.h
+++ pslib.h
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 19:32:42 by htrindad          #+#    #+#             */
-/*   Updated: 2024/09/17 11:10:34 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/09/17 11:38:20 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,17 @@ typedef struct s_stack
 {
 	int				val;
 	int				pos;
-	bool			above_median;
-	bool			cheapest;
-	struct s_stack	*target;
 	struct s_stack	*prev;
 	struct s_stack	*next;
 }	t_stack;
 
 char	**ps_split(char *str, char sep);
 long	ps_atol(char *num);
-void	*ps_stacknew(int val);
 size_t	ps_strlen(char const *s);
 char	*ps_substr(char const *s);
 char	*ps_substr(char const *s, unsigned int start, unsigned int end);
 char	*ps_strdup(char const *s);
 size_t	ps_strlen(char const *s);
 char	*ps_substr(char const *s, unsigned int start, unsigned int len);
-int		ps_stacklen(t_stack *stack);
 
 #endif
